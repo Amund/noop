1b4a3c38001c69e4983509edc459056e4039b470
Amund <mailamund@gmail.com>
2015-09-12 23:51:41 +0200

    Updated sample headers

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 
-//setlocale( LC_ALL, array( 'fr_FR.UTF-8', 'fr_FR', 'fr' ) );
-//ini_set( 'date.timezone', 'Europe/Paris' );
+// Some examples of useful directives to add :
+// setlocale( LC_ALL, array( 'fr_FR.UTF-8', 'fr_FR', 'fr' ) );
+// ini_set( 'date.timezone', 'Europe/Paris' );
+// header_remove( 'X-Powered-By' );
 
 require 'noop.php';
 noop::start();
